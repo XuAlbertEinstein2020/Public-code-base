8d01d9b0849e73759a376bb2558461cb2dd4d1ea
XuAlbertEinstein2020 <72545708+XuAlbertEinstein2020@users.noreply.github.com>
2021-09-11 11:53:29 +0800

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,33 +1,30 @@
 #include <cstdio>
 #include <iostream>
 using namespace std;
-int n,a[10005],m,sum,cnt=0,nmax;
-int fun(int p,int b){
+long long n,a[1000005],m,sum,cnt=0,nmax;
+int fun(long long p,long long b){
 	if(b==0)	return a[p];
 	else	return a[p]-a[b-1];
 }
 int main(){
-	int left,right,middle;
+	long long left,right,middle;
 	cin>>n;
-	for(int i=0;i<n;i++)	cin>>a[i];
-	for(int i=1;i<n;i++)	a[i]+=a[i-1];
+	for(long long i=0;i<n;i++)	cin>>a[i];
+	for(long long i=1;i<n;i++)	a[i]+=a[i-1];
 	cin>>m;
-	for(int i=0;i<n;i++){
-		left=i,right=n-1,nmax=0;
+	for(long long i=0;i<n;i++){
+		left=i,right=n-1,nmax=-1;
 		while(left<=right){
 			middle=(left+right)/2;
 			//cout<<fun(middle,i)<<" "<<middle<<endl;
 			if(fun(middle,i)>m){
 				right=middle-1;
-			}else if(fun(middle,i)==m){
-				nmax=nmax>middle?nmax:middle;
-				break;
 			}else{
 				left=middle+1;
 				nmax=nmax>middle?nmax:middle;
 			}
-		};
-		if(nmax!=0)	cnt+=(nmax-i+1);
+		}
+		if(nmax!=-1)	cnt+=(nmax-i+1);
 	}cout<<cnt;
 	return 0;
 }
